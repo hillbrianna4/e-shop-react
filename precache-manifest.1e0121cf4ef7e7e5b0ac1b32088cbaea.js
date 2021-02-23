ab2fc983c44bc397ac6f69614fe831150fdf342c
Brianna Hill <ThatBrianna@Brianna-Hills-iMac.local>
2021-02-23 08:48:26 -0800

    Updates

---
+++ precache-manifest.1e0121cf4ef7e7e5b0ac1b32088cbaea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625941de2f930d2a77d42ab2efc95ff1",
+    "url": "/e-shop-react/index.html"
+  },
+  {
+    "revision": "755540a9ec2395e47c01",
+    "url": "/e-shop-react/static/js/2.9ad802f7.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/e-shop-react/static/js/2.9ad802f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493c48a4d7ad47289285",
+    "url": "/e-shop-react/static/js/main.6843fc87.chunk.js"
+  },
+  {
+    "revision": "b711f4cc6d708eb83db9",
+    "url": "/e-shop-react/static/js/runtime-main.63fe85ef.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/e-shop-react/static/media/commerce.422fec10.png"
+  }
+]);
